32653ab8b5e59d8364675e390b4e0fa5922ca0f3
RishabhKankani <rishabh.kankani@ripplehire.com>
2021-01-20 17:45:49 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponentComponent } from './Components/first-component/first-component.component';
+import { FourthComponentComponent } from './Components/fourth-component/fourth-component.component';
+import { SecondComponentComponent } from './Components/second-component/second-component.component';
+import { ThirdComponentComponent } from './Components/third-component/third-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first', component: FirstComponentComponent },
+  {path: 'second', component: SecondComponentComponent},
+  {path: 'third', component: ThirdComponentComponent},
+  {path: 'fourth', component: FourthComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FirstComponentComponent, SecondComponentComponent, ThirdComponentComponent, FourthComponentComponent]
